556dab42e3b079452ec2250d535665d4c6d3cfe8
Natalia Colmenares <38693615+nataliacolm@users.noreply.github.com>
2022-04-13 14:50:05 -0400

    adds problem 2 details

--- problem2/Temp.java
+++ problem2/Temp.java
@@ -4,159 +4,199 @@
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicIntegerArray;
 
 public class Temp {
     private static final int min = -100;
     private static final int max = 70;
-    private static final int hours = 4;
-    private static final int iterations = hours * 60; // 60 iterations for 1 hour. Consider 1 iteration a minute.
+    private static final int hours = 20;
     private static final int sensors = 8;
+    private static final int num_iterations_in_hour = 480;
+    private static final int num_iterations_in_10_min = 80;
+    private static final int num_iterations_per_thread = 60;
+
 
     private static SensorList list = new SensorList(min, max); // include temperature range here.
     private static SensorList interval_list = new SensorList(min, max); // include temperature range here.
     private static AtomicIntegerArray intervals = new AtomicIntegerArray(480);
 
-    private static AtomicInteger num_iterations = new AtomicInteger(0);
     private static CountDownLatch cLatch = new CountDownLatch(8);
     private static AtomicInteger index = new AtomicInteger(0);
+    private static AtomicInteger ten_min = new AtomicInteger(0);
 
     private static Random rand_temp = new Random();
-    private static volatile boolean ready = false;
+    private static AtomicBoolean ready = new AtomicBoolean();
 
     static class Sensor implements Runnable
     {
-        // Each sensor has a state to "synchronize" them when a minute is reached.
-        public volatile boolean state = false;
+        private int name;
+        private ThreadLocal<Integer> num_iterations = new ThreadLocal<Integer>();
+
+        public Sensor(int name)
+        {
+            this.name = name;
+        }
 
         @Override
         public void run()
         {
-            while (!ready)
+            num_iterations.set(0);
+
+            while (num_iterations.get() < num_iterations_per_thread)
             {
-                // wait until it marks a minute
-                while(!state)
-                {
-                    if (ready)
-                        return;
-                }
-
-                // If a thread reaches here, a minute is up and it is time to add it to the shared memory space.
-                state = false;
-
                 int curr_temp = rand_temp.nextInt((max + 1) - (min)) + min;
 
-                list.add(curr_temp); // Look at the add method in SensorList to see why a thread will not be delayed here!
-                interval_list.add(curr_temp);
-                // Keep track of numbers added to interval list
-                intervals.set(index.getAndIncrement(), curr_temp);
-                cLatch.countDown();
+                // Add 80 items to represent adding to the first 10 minutes. Therefore, each thread only
+                // input 10 times.
+                if (num_iterations.get() < 10)
+                {
+                    // get first ten minutes added to linked list
+                    interval_list.add(curr_temp);
+
+                    // ensure that every thread has inputted their own readings for
+                    // the 10 minutes is counted.
+                    ten_min.getAndIncrement();
+                }
+
+                // Add to the other lists, but not the 10 minute window anymore, main will track that when new
+                // temperature readings are added to the intervals arraylist.
+                if (num_iterations.get() < num_iterations_per_thread)
+                {
+                    // make sure sensors add in correct position to avoid missing a minute reading
+                    intervals.set(name, curr_temp);
+                    this.name = name + sensors;
+
+                    list.add(curr_temp);
+                    // used to demonstrate a new temperature reading has been added.
+                    index.getAndIncrement();
+
+                }
+
+                num_iterations.set(num_iterations.get() + 1);
             }
+
+            ready.set(true);
+            cLatch.countDown();
         }
     }
 
     public static void main(String [] args)
     {
-        ArrayList<Sensor> arr_list = new ArrayList<>();
         list.create_list(); // list stores info for the hour
         interval_list.create_list(); // interval list stores info for 10 minutes.
+        ExecutorService pool = Executors.newFixedThreadPool(sensors);
+
+        long startTime = System.nanoTime();
     
         for (int i = 0; i < sensors; i++)
         {
-            Sensor sensor = new Sensor();
-            arr_list.add(sensor);
-            Thread thread = new Thread(sensor, Integer.toString(i)); // give the name of the thread as index
-            thread.start();
+            pool.execute(new Sensor(i));
         }
 
         int hour = 1;
         int max = -1000; // dummy value
         int max_interval = -1000; // dummy value 
-        int num_iteration_in_hour = 0;
         int interval_index_list = 0;
+        int time = 0;
 
-        while (num_iterations.get() < iterations)
+        while (hour <= hours)
         {
-            // attempt to synchronize threads
-            for (int i = 0; i < sensors; i++)
+            // Do productive work when the threads get a new temperature reading.
+            // 80 iterations marks 10 minutes! Start looking for the largest 10 minute interval window.
+            if (ten_min.get() >= num_iterations_in_10_min)
             {
-                Sensor temp = arr_list.get(i);
-                temp.state = true;
-            }
-
-            if (num_iteration_in_hour == 9)
-            {
-                int min_temp = interval_list.find_min();
-                int max_temp = interval_list.find_max();
-
-                if ((max_temp - min_temp) > max)
+                // compare first ten minutes
+                if (time == 0)
                 {
-                    max = max_temp - min_temp;
-                    max_interval = num_iteration_in_hour + 1;
+                    int min_temp = interval_list.find_min();
+                    int max_temp = interval_list.find_max();
+
+                    time = num_iterations_in_10_min;
+
+                    if ((max_temp - min_temp) > max)
+                    {
+                        max = max_temp - min_temp;
+                        max_interval = time / sensors;
+                    }
+                }
+
+                // Keep removing 8 indices and adding 8 indices to update the 10 minute interval window.
+                else if (time + sensors <= index.get())
+                {
+                    int count = 0;
+                    // start removing intervals that do not work for the next iteration.
+                    while (count < sensors)
+                    {
+                        interval_list.remove(intervals.get(interval_index_list));
+                        interval_list.add(intervals.get(time));
+                        interval_index_list++;
+                        count++;
+                        time++;
+                    }
+
+                    // Find the largest range within the window.
+                    int min_temp = interval_list.find_min();
+                    int max_temp = interval_list.find_max();
+    
+                    if ((max_temp - min_temp) > max)
+                    {
+                        max = max_temp - min_temp;
+                        max_interval = time / sensors;
+                    }
                 }
             }
 
-            else if (num_iteration_in_hour >= 10)
+            if (ready.get() == true && time >= num_iterations_in_hour)
             {
-                int count = 0;
-                // start removing intervals that do not work for the next iteration.
-                while (count < 8)
+                try
                 {
-                    interval_list.remove(intervals.get(interval_index_list));
-                    interval_index_list++;
-                    count++;
+                    cLatch.await();
                 }
-                int min_temp = interval_list.find_min();
-                int max_temp = interval_list.find_max();
 
-                if ((max_temp - min_temp) > max)
+                catch(Exception e)
                 {
-                    max = max_temp - min_temp;
-                    max_interval = num_iteration_in_hour + 1;
+                    ;
                 }
-            }
-  
-            num_iterations.getAndIncrement();
-            num_iteration_in_hour++;
 
+                cLatch = new CountDownLatch(8);
 
-            // Make sure sensors give time at the correct minute by synchronizing them with a count down!
-            try
-            {
-                cLatch.await();
-            }
-
-            catch(Exception e)
-            {
-                ;
-            }
-
-            cLatch = new CountDownLatch(8);
-
-            if (num_iterations.get() % 60 == 0)
-            {
                  // REPORT SECTION:
                 System.out.println("==========TOP 5 HIGHEST TEMPS: HOUR " + hour +"============");
                 list.get_top_five();
                 System.out.println("==========TOP 5 LOWEST TEMPS: HOUR " + hour +"=============");
                 list.get_low_five();
 
-                System.out.println("+++++ MAX 10 MINUTE INTERVAL: " + max_interval + "-" + (max_interval - 10) + " Difference: " + max +" +++++");
+                System.out.println("+++++ MAX 10 MINUTE INTERVAL: " + max_interval + " - " + (max_interval - 10) + " Difference: " + max +" +++++");
                 System.out.println();
                 System.out.println();
 
-                list.cleanup();
                 hour++;
-                max = -1000;
-                num_iteration_in_hour = 0;
-                interval_index_list = 0;
-                index.set(0);
-                interval_list.cleanup();
+
+                if (hour <= hours)
+                {
+                    list.cleanup();
+                    max = -1000;
+                    interval_index_list = 0;
+                    index.set(0);
+                    interval_list.cleanup();
+                    ready.set(false);
+                    time = 0;
+                    ten_min.set(0);
+
+                    for (int i = 0; i < sensors; i++)
+                    {
+                        pool.execute(new Sensor(i));
+                    }
+                }
             }
-           
         }
 
-        ready = true;
+        pool.shutdown();
+        long elapsedTime = System.nanoTime() - startTime;
+        System.out.println("Execution time (milliseconds): " + elapsedTime / 1000000);
     }
 }
